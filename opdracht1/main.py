f2001a92e1cbefb18058471bb28a01f832573569
Northie <tomnoordermeer@live.nl>
2023-03-25 14:47:52 +0100

    opdarcht werkent, compileert, traint en predicts

--- opdracht1/main.py
+++ opdracht1/main.py
@@ -12,16 +12,22 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 from PIL import Image
 
+
+# annotaties inladen met panda
 test_data = pd.read_csv("smoke\\test\\_annotations.csv")
 train_data = pd.read_csv("smoke\\train\\_annotations.csv")
 valid_data = pd.read_csv("smoke\\valid\\_annotations.csv")
 
+#zet de resize waardes voor hoogte en breedte
 height = 224
 width = 224
 
+#de originele waardes voor hoogte en breedte
 og_height = 480
 og_width = 640
 
+
+#laden van de foto's en ze gelijk resizen
 def load(path):
     img = load_img(path, target_size=(height, width))
     img = img.resize((224,224))
@@ -29,15 +35,19 @@ def load(path):
     img = img / 255.0
     return img
 
+# terug zetten naar de orginele waardes
 def normalize(img):
     return cv2.resize(img, (og_width, og_height))
 
+#label waardes omzetten naar text om te kunnen plaatsen in de plaatjes
 def CheckClass(label):
     if label == 0:
         return 'Smoke'
     else:
         return 'No Smoke'
 
+# laden van de plaatjes en het resizen naar de target grootte voor train, test en validatie plaatjes
+# laden van de bounding box area en convertie van labels naar nummers voor train, test en validatie plaatjes
 train_img = np.array([cv2.imread('smoke\\train\\' + filename) for filename in train_data['filename']])
 train_img = np.array([cv2.resize(img, (height, width)) for img in train_img])
 train_name = np.array([filename for filename in train_data['filename']])
@@ -56,10 +66,12 @@ valid_name = np.array([filename for filename in valid_data['filename']])
 valid_bound = valid_data[['xmin', 'ymin', 'xmax', 'ymax']].values
 valid_labels = valid_data['class'].map({'smoke':0, 'no_smoke':1}).values
 
+# convertie van labels naar categoricale vorm
 train_cls = to_categorical(train_labels, 2)
 test_cls = to_categorical(test_labels, 2)
 valid_cls = to_categorical(valid_labels, 2)
 
+# dictionaries aanmaken voor de targets
 trainTargets = {
     "class_label": train_cls,
     "bounding_box": train_bound
@@ -75,22 +87,25 @@ validTargets = {
     "bounding_box": valid_bound
 }
 
+#defineren van de loss functies voor elke output van het model
 losses = {
     "class_label": "categorical_crossentropy",
     "bounding_box": "mse",
 }
 
+#defineren van weight loss voor elke functie
 lossWeights = {
     "class_label": 1.0,
     "bounding_box" : 1.0
 }
 
-opt = Adam()
 
+#VGG model laden en flatten
 vgg = VGG16(weights='imagenet', include_top=False, input_tensor=Input(shape=(224,224,3)))
 vgg.trainable = False
 flatten = Flatten()(vgg.output)
 
+#defineren van de output layers en de boundingbox
 bbox_head = Dense(128, activation='relu') (flatten)
 bbox_head = Dense(64, activation='relu') (bbox_head)
 bbox_head = Dense(32, activation='relu') (bbox_head)
@@ -102,21 +117,26 @@ softmax_head = Dense(512, activation='relu')(softmax_head)
 softmax_head = Dropout(0.5)(softmax_head)
 softmax_head = Dense(2, activation='softmax', name="class_label")(softmax_head)
 
+#model maken
 model = Model( inputs = vgg.input, outputs = (softmax_head, bbox_head))
 
-model.compile(loss= losses, optimizer=opt,metrics=["accuracy"], loss_weights=lossWeights)
+#compileren van het model
+model.compile(loss= losses, optimizer=Adam(),metrics=["accuracy"], loss_weights=lossWeights)
 
+#data converteren naar float32
 train_labels = train_labels.astype('float32')
 val_labels = valid_labels.astype('float32')
 
+#model trainen
 model.fit(train_img, trainTargets, epochs = 1, validation_data=(test_img, testTargets))
 
+#model evalueren
 res = model.evaluate(test_img, testTargets)
-print('Test loss:', res[0])
-print('Test accuracy:', res[1])
 
+#predictions maken voor de nieuwe images
 predLab, predBox = model.predict(valid_img)
 
+#laten zien van de data
 for i in range(len(valid_img)):
     fig, ax = plt.subplots()
     img = valid_img[i]
